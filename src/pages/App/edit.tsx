f19077579d42a3287699e06b8cfdaee2c5f03286
heyu <1395294694@qq.com>
2022-10-27 21:39:13 +0800

    feat: 加载组件规格实例

--- src/pages/App/edit.tsx
+++ src/pages/App/edit.tsx
@@ -1,11 +1,19 @@
 import { useRef, useEffect } from 'react'
 import { observer } from 'mobx-react';
 import engine from 'vitis-lowcode-engine'
+import { getComponents } from '../../services/businessUnit'
+import stores from '../../stores'
 
 export default observer(() => {
   const container = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    engine.init(container.current)
+    engine.init(container.current);
+    (async () => {
+      if (stores.app.BUName) {
+        const result = await getComponents(stores.app.BUName);
+        engine.material.load(result.map(item => ({ npm: item.packageName, version: item.version })));
+      }
+    })();
   }, []);
 
   return <div ref={container} style={{height: '100%'}} >ddd</div>
